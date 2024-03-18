466abd1d2c12322dab1b927726cf1efc5917cf53
M-zain-9999 <141211040+M-zain-9999@users.noreply.github.com>
2024-03-18 17:13:41 +0500

    Time: 743 ms (68.57%) | Memory: 25 MB (90.48%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.swift
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.swift
@@ -2,17 +2,17 @@ class Solution {
     func findMinArrowShots(_ points: [[Int]]) -> Int {
         guard !points.isEmpty else { return 0 }
     
-    let sortedPoints = points.sorted { $0[1] < $1[1] }
+    let sortedpoints = points.sorted { $0[1] < $1[1] }
     
     var arrows = 1
-    var currentEnd = sortedPoints[0][1]
+    var cEnd = sortedpoints[0][1]
     
-    for i in 1..<sortedPoints.count {
-        let balloon = sortedPoints[i]
+    for i in 1..<sortedpoints.count {
+        let b = sortedpoints[i]
         
-        if balloon[0] > currentEnd {
+        if b[0] > cEnd {
             arrows += 1
-            currentEnd = balloon[1]
+            cEnd = b[1]
         }
     }
     
